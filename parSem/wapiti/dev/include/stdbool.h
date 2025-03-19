2b48350a121227fb21aade0920a9844f1e53a1dc
claude.roux@naverlabs.com <roux@chaos-03.int.europe.naverlabs.com>
2025-03-19 21:15:02 +0100

    update

--- parSem/wapiti/dev/include/stdbool.h
+++
@@ -1,8 +0,0 @@
-#ifndef STDBOOL
-#define STDBOOL
-
-#define bool char
-#define true 1
-#define false 0
-
-#endif
--- parSem/xml/dev/libxml2-2.6.28/config.h
+++
@@ -1,5 +0,0 @@
-#ifdef _WIN32
-#include "include/win32config.h"
-#else
-#include "config.h.unix"
-#endif
